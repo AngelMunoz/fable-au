a751ef7bd2cac9cb024f582e1c807eb9e3bcfcda
Angel D. Munoz <scythes.lol@gmail.com>
2023-02-27 16:42:40 -0600

    add some DI and shoelace

--- src/Main.fs
+++ src/Main.fs
@@ -8,8 +8,19 @@ open Aurelia.Decorators
 
 open AppRoot
 open MyCounter
+open Fable.Core.JS
+
+importSideEffects "@shoelace-style/shoelace/dist/shoelace.js"
+importSideEffects "@shoelace-style/shoelace/dist/themes/light.css"
+importSideEffects "@shoelace-style/shoelace/dist/themes/dark.css"
 
 Aurelia
+    .register(
+        Aurelia.LoggerConfiguration.create (
+            {| level = 0
+               sinks = [| ConsoleSink |] |}
+        )
+    )
     .register(jsConstructor<MyCounter>)
     .register(jsConstructor<JsonValueConverter>)
     .app(jsConstructor<AppRoot>)
--- src/MyCounter.fs
+++ src/MyCounter.fs
@@ -3,18 +3,33 @@ module MyCounter
 open Fable.Core
 
 open Fable.Core.JsInterop
+
+open Aurelia
 open type Aurelia.Decorators
 
+
+
 [<AttachMembers>]
-type MyCounter() =
+type MyCounter(logger: ILogger) =
+
+
+    let _logger = logger.scopeTo (nameof (MyCounter))
     let mutable counter = 0
+    do logger.info ("olv")
+    static member inject = [| jsConstructor<ILogger> |]
 
     member _.increment() : unit = counter <- counter + 1
 
     member _.decrement() : unit = counter <- counter - 1
 
+    member _.counterChanged(newValue: int, oldValue: int) : unit =
+        logger.info ($"{newValue} - {oldValue}")
+        _logger.info ($"{newValue} - {oldValue}")
+
+
 
 CustomElement
     ({| template = importDefault "./my-counter.html"
-        name = "my-counter" |})
+        name = "my-counter"
+        bindables = [| "counter" |] |})
     jsConstructor<MyCounter>
