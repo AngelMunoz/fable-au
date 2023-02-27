467c34dcc59bcfd1ccd895beb1c4a3486dfc329f
Angel D. Munoz <scythes.lol@gmail.com>
2023-02-27 12:03:56 -0600

    remove js

--- src/Main.js
+++
@@ -1,7 +0,0 @@
-import aurelia from "aurelia";
-import { MyCounter } from "./MyCounter.js";
-import { DecoratorsModule_JsonValueConverter } from "./Aurelia.js";
-import { AppRoot } from "./AppRoot.js";
-
-aurelia.register(MyCounter).register(DecoratorsModule_JsonValueConverter).app(AppRoot).start();
-
